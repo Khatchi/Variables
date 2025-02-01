dfe709b866290c61eb1cf3319358a3f06f782781
Khatchi <solomon9ice2014@gmail.com>
2025-02-01 23:47:49 +0100

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //int x;
+        //int y;
+
+        //x = 7;
+        //y = x + 3;
+
+        //Console.WriteLine(y);
+
+        //Console.ReadLine();
+
+        Console.WriteLine("What's your name? ");
+        Console.Write("Type your first name: ");
+
+        string myFirstName;
+        myFirstName = Console.ReadLine();
+
+        string myLastName;
+        Console.Write("Type your last name: ");
+        myLastName = Console.ReadLine();
+
+        Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+
+    }
+}
